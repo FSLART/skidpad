c735284502825e07fe07dac477c6a6a9954eebe1
Gabriel Januário Tomás <gabrieljtomas@gmail.com>
2026-07-02 17:22:36 +0100

    forget sometings

--- include/my_node.hpp
+++ include/my_node.hpp
@@ -27,6 +27,9 @@ class skidpad_node : public rclcpp::Node
         CarData carData;
         lart_msgs::msg::ConeArray::SharedPtr coneArray;
         const double middleCar = 0.7;
+        double prev_corr_x_ = 0.0;
+        double prev_corr_y_ = 0.0;
+
 
         rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_vis_pub;
         rclcpp::Publisher<lart_msgs::msg::PathSpline>::SharedPtr path_control_pub; 
